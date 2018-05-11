b1c640afea91a421f00864a92fa75d7015d11c56
Unit-Origami <origami.unit@gmail.com>
2018-05-11 14:04:17 +0900

    型定義修正

--- mytypes/my-config.d.ts
+++ mytypes/my-config.d.ts
@@ -2,18 +2,21 @@ import * as config from 'config'
 import * as Sequelize from 'sequelize'
 declare let my_config;
 declare namespace my_config {
-  interface params {}
   // sequelize設定
-  interface sequelize_config extends params {
-    readonly database?:string;
-    readonly username?:string;
-    readonly password?:string;
-    readonly options?:Sequelize.Options;
+  interface sequelize_config {
+    readonly database?: string;
+    readonly username?: string;
+    readonly password?: string;
+    readonly options?: Sequelize.Options;
   }
   // jwt設定
-  interface jwt_config extends params {
+  interface jwt_config {
     readonly authentication_secret_key: string;
     readonly algorithm: string;
+  }
+  // その他
+  interface other {
+    readonly [index: string]: any;
   }
 }
 export = my_config;
