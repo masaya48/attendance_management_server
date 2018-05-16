0423b87406a2cbbff97ad2ee186e7e8bd7503965
Unit-Origami <origami.unit@gmail.com>
2018-05-16 13:15:07 +0900

    singletonテスト

---
+++ src/test/test.ts
@@ -0,0 +1,5 @@
+import * as Config from 'config'
+import S from './../libs/dbconn'
+
+const seq1 = S
+export default seq1
---
+++ src/test/test2.ts
@@ -0,0 +1,4 @@
+import S from './../libs/dbconn'
+import seq1 from './test'
+const seq2 = S
+console.log(seq1 === seq2)
