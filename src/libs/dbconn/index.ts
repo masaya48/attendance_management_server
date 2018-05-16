4175fb10bc2449583510063da6db808d8b142c40
Unit-Origami <origami.unit@gmail.com>
2018-05-16 14:34:44 +0900

    コメント変更

--- src/libs/dbconn/index.ts
+++ src/libs/dbconn/index.ts
@@ -1,4 +1,4 @@
-import * as Config from 'config'
+import * as config from 'config'
 import * as Sequelize from 'sequelize'
 import * as my_config from 'my-config'
 /**
@@ -9,21 +9,19 @@ import * as my_config from 'my-config'
  *   options:  <Object>
  * }
  */
-//export default function dbconn(config:Config.IConfig) {
-  let params = Config.get<my_config.sequelize_config>('sequelize')
-  if (!params || !params.options) {
-    throw new Error('configration parameters not found.')
-  }
+let params = config.get<my_config.sequelize_config>('sequelize')
+if (!params || !params.options) {
+  throw new Error('configration parameters not found.')
+}
 
-  console.log(
-      'Sequelize connecting to %s on %s as %s',
-      params.database, params.options.host || 'localhost', params.username)
+console.log(
+    'Sequelize connecting to %s on %s as %s',
+    params.database, params.options.host || 'localhost', params.username)
 
-  const sequelize = new Sequelize(
-    params.database,
-    params.username,
-    params.password,
-    params.options)
+const sequelize = new Sequelize(
+  params.database,
+  params.username,
+  params.password,
+  params.options)
 
-  export default sequelize
-//}
+export default sequelize
