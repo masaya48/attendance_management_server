2d3aa6720c49e6cbbd01dd2b51c46a7916db73bc
Unit-Origami <origami.unit@gmail.com>
2018-05-30 09:16:17 +0900

    ヘッダー追加

--- src/app.ts
+++ src/app.ts
@@ -29,11 +29,11 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, './../public')))
 
 // 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-//   next()
-// })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 // url mappings
 app.use('/api', routes())
