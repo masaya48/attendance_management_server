ac31b6f639458b06858ec55909f9998d772fc15a
Unit-Origami <origami.unit@gmail.com>
2018-05-31 08:33:17 +0900

    model修正

--- src/models/types/m_absent_type.d.ts
+++ src/models/types/m_absent_type.d.ts
@@ -1,14 +1,14 @@
-import * as Sequelize from 'sequelize';
+import * as Sequelize from 'sequelize'
 declare namespace MAbsentType {
   interface Params {
     absent_type: number
     absent_name: string
   }
-  interface InstanceParams {
+  interface InstanceParams extends Params {
     created_at: string
     updated_at: string
   }
-  interface Instance extends Sequelize.Instance<InstanceParams & Params>, InstanceParams {
+  interface Instance extends Sequelize.Instance<InstanceParams>, InstanceParams {
   }
   interface Model extends Sequelize.Model<Instance, Params> {
   }
