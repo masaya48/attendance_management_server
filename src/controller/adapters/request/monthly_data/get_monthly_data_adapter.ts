f51fa4540625303f390eedb17a07918b217db1d1
y_imori <mt.north96@gmail.com>
2018-06-28 00:55:39 +0900

    frontに合わせて修正

--- src/controller/adapters/request/monthly_data/get_monthly_data_adapter.ts
+++ src/controller/adapters/request/monthly_data/get_monthly_data_adapter.ts
@@ -4,17 +4,21 @@ import * as Express from 'express'
 import RequestAdapter from './../request_adapter'
 import MonthlyDataRequestDTO from './../../../../domain/dto/request/monthly_data/monthly_data_get_request'
 import BaseRequestDTO from './../../../../domain/dto/request/base_request_dto'
+import MyLoginRequest from 'my_request'
+import { compareSync } from 'bcrypt';
 class MonthlyDataRequestAdapter implements RequestAdapter {
   public convert(req: MonthlyDataRequest): MonthlyDataRequestDTO {
-    const user_no = req.body.user_no
-    const month = req.body.month
+    const user_no = req.user.user_no
+    const month = req.body.get_data_month
+    console.log('here00')
+    console.log(user_no)
+    console.log(month)
     return new MonthlyDataRequestDTO(user_no, month)
   }
 }
-interface MonthlyDataRequest extends Express.Request {
+interface MonthlyDataRequest extends MyLoginRequest {
   body: {
-    user_no: number,
-    month: Date,
+    get_data_month: Date,
   }
 }
 export default MonthlyDataRequestAdapter
