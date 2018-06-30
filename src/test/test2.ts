447e74086377ba586c779df890eb1658709b9783
y_imori <mt.north96@gmail.com>
2018-06-30 09:25:58 +0900

    コンパイルエラー解消

--- src/test/test2.ts
+++ src/test/test2.ts
@@ -1,5 +1,5 @@
-import Service from './../domain/services/office_hours_service'
-const service = new Service()
+// import Service from './../domain/services/office_hours_service'
+// const service = new Service()
 
-const A = service.checkAttendance(1)
+// const A = service.checkAttendance(1)
 
